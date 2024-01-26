a8f5744d18777428f1432ca6b71619c8da057e9f
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-01-26 22:51:13 +0800

    UpdatedResultView

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showModel = false   // set my state
+    
     var body: some View {
-            Text("")
+        
+        ZStack{
+            
+            VStack{
+                // MARK: Game Tietle
+                Text("Tic-Tac-Toe")
+                    .font(.system(size: 28, weight:  .medium))
+                    .padding(.bottom, 22)
+                
+                Spacer()
+                
+                // MARK: Game Board
+                Text("Game Board")
+                
+                Button {
+                    
+                    //action
+                    showModel = true
+                    
+                } label: {
+                    Text("Click Meow")
+                        .frame(width:170, height: 50)
+                        .cornerRadius(12)
+                        .background(.pink)
+                        .padding(20)
+                        
+                }
+                
+                Spacer()
+            }
+            
+            // Game Result Modal
+            
+            if (showModel == true) {
+                GameResultView()
+            }
+            
         }
+        
+    }
     }
 
 
