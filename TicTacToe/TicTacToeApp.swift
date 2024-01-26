acc77e9659c7e9680acde54fb2588e2d49b9d7e7
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-01-26 21:21:42 +0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by yucian huang on 2024/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
